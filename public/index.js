fe0381599429f110dedfca36aeef2419b72d7586
abdi warsame <abdiwarsame@hotmail.co.uk>
2020-02-04 21:30:04 +0000

    camera access

--- public/index.js
+++ public/index.js
@@ -1,1 +1,23 @@
-console.log("Hello bitches!");
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
+
+// Elements for taking the snapshot
+var snapshot = document.getElementById('snapshot');
+var context = snapshot.getContext('2d');
+var video = document.getElementById('videoElement');
+
+// Trigger photo take
+document.getElementById("takePhoto").addEventListener("click", function() {
+    context.drawImage(video, 0, 0, 640, 480);
+    let snapData = context.getImageData(0, 0, 640, 480);
+    console.log(snapData)
+});
